a5ee069bf54fdd0e2f451c1d576eabe3fe5431ab
yangshaotong <yangshaotong.baidu.com>
2023-03-03 19:56:49 +0800

    增加node版本验证

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -1,14 +1,26 @@
 const { log, locale } = require('@heyday-cli/utils');
 const pkg = require('../package.json');
-
+const { LOWEST_NODE_VERSION } = require('./const');
+const semver = require('semver'); // 比对版本号
+const colors = require('colors/safe'); // 输出带颜色的字体
 
 module.exports = core;
 
 function core() {
-    checkPkgVersion();
+    welcome();
+    checkNodeVersion();
 }
 
-function checkPkgVersion() {
+// 检查node版本，保证脚手架可以正常运行
+function checkNodeVersion() {
+    // 获取当前版本号
+    const curNodeVersion = process.version;
+    if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
+        throw new Error(colors.red(`heyday-cli 需要安装 v${LOWEST_NODE_VERSION} 以上版本的 Node.js`));
+    }
+}
+
+function welcome() {
     log.notice('cli', pkg.version);
     log.success(locale());
 }
