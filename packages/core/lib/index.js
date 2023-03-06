ac3c802a666faa76468d8eec6b2317f95b7e3a52
yangshaotong <yangshaotong.baidu.com>
2023-03-06 15:28:12 +0800

    feat: 增加root账号检测和主目录检测

--- packages/core/lib/index.js
+++ packages/core/lib/index.js
@@ -3,6 +3,9 @@ const pkg = require('../package.json');
 const { LOWEST_NODE_VERSION } = require('./const');
 const semver = require('semver'); // 比对版本号
 const colors = require('colors/safe'); // 输出带颜色的字体
+const rootCheck = require('root-check'); // 降级root账户
+const userHome = require('user-home'); // 获取用户主目录
+const fs = require('fs'); // node文件操作对象
 
 module.exports = core;
 
@@ -10,8 +13,18 @@ function core() {
     try {
         welcome();
         checkNodeVersion();
+        // 如何是以root启动安装，则对root降级，防止以后用其他用户登录无法访问或操作的情况出现
+        rootCheck(colors.red('请避免使用 root 账户启动本应用'));
+        checkUserHome();
     } catch (e) {
         log.error(e.message);
+    }
+}
+
+// 缓存等操作依赖用户主目录，预检查用户主目录
+function checkUserHome() {
+    if (!userHome || !fs.existsSync(userHome)) {
+        throw new Error(colors.red('当前登录用户主目录不存在！'));
     }
 }
 
@@ -20,7 +33,7 @@ function checkNodeVersion() {
     // 获取当前版本号
     const curNodeVersion = process.version;
     if (!semver.gte(process.version, LOWEST_NODE_VERSION)) {
-        throw new Error(colors.red(`heyday-cli 需要安装 v${LOWEST_NODE_VERSION} 以上版本的 Node.js`));
+        throw new Error(colors.red(`「heyday-cli」需要安装「v${LOWEST_NODE_VERSION}」以上版本的Node.js`));
     }
 }
 
